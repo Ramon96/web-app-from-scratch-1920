c75827393f0440343faed3e1cf24dc7f29dc12bd
Ramon96 <rfm1@live.nl>
2020-02-24 13:29:19 +0100

    updated key

--- docs/js/app.js
+++ docs/js/app.js
@@ -12,7 +12,7 @@ function init(){
             .then(function(data){
                 removeDom(document.getElementsByTagName('main')[0]);
                 data.forEach(match => {
-                createMatchNode(match.time, "p", document.getElementsByTagName('main')[0]);
+                    createMatchNode(match.time, "p", document.getElementsByTagName('main')[0]);
                 })
             })
     });
--- docs/js/modules/getData.js
+++ docs/js/modules/getData.js
@@ -5,14 +5,14 @@ import { fetchMatchHistory } from "./helpers/fetchMatchHistory.js";
 //retrieves and cleans the data by league of legends summoner ID
 async function getData(name){
 //     // incase of 403, it may be that the key has been expired (24 hours)
-    const apiKey = "RGAPI-15f7de9a-03b3-4b8f-b641-a09bd8e8b0a4";
+    const apiKey = "RGAPI-b454aa23-b8b5-4284-80a0-a93007b60d6c";
     const url = `https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${apiKey}`;
 
 
     const summonerInformation = await fetchSummoner(url);
     const matchHistory = await fetchMatchHistory(summonerInformation, apiKey);
     const cleanData = cleanUp(matchHistory)
-
+    console.log(cleanData);
     return cleanData;
 }
 
@@ -25,7 +25,7 @@ function cleanUp(matchHistory){
             time: convertTimestamp(key.timestamp, key.platformId),
             lane: key.lane,
             premade: key.role,
-            queue: key.queue,
+            queue: key.queue, 
             season: key.season
         }   
     })
